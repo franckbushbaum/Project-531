1f703929299e9c62849438af40900d0cd4f3319d
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-10-12 14:32:59 -0500

    created basic database layout in database.sql.

--- database.sql
+++ database.sql
@@ -8,3 +8,26 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) UNIQUE NOT NULL,
+);
+
+CREATE TABLE "workout_type" (
+    "id" SERIAL PRIMARY KEY,
+    "description" NUMERIC NOT NULL,
+);
+
+CREATE TABLE "workout" (
+    "workout_id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user" NOT NULL,
+    "workout_type_id" INT REFERENCES "workout_type",
+    "one_rep_max" INT NOT NULL,
+    "week" INT NOT NULL,
+    "weight" INT NOT NULL,
+    "set" INT NOT NULL,
+    "reps" INT NOT NULL,
+    "created_at" DATE,
+
+);
--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -8,6 +10,7 @@ import React from 'react';
 function InfoPage() {
   return (
     <div className="container">
+      <Button variant="contained">Hello World</Button>
       <p>Info Page</p>
     </div>
   );
