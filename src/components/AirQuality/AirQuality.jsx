50e79f556d82493d0f7201d831d5a1489c556423
Franck Bushbaum <franck.bushbaum@yahoo.com>
2022-04-07 06:28:29 -0500

    Grid style implemented in air quality box.

--- src/components/AirQuality/AirQuality.jsx
+++ src/components/AirQuality/AirQuality.jsx
@@ -29,29 +29,45 @@ function AirQuality({ hour }) {
 }, [])
 
   return (
-    <>
+    
       <div className="hours">
-        <div>
-          <div className="modal-title">{(moment(hour.timestamp_local).calendar())}</div>
-            <div className="aqi-display">
-                <div className="aqi-display-number">
-                  AQI<br />{hour.aqi}
-            </div>
-            </div>
-              {/* //2  Air Quality Index [US - EPA standard 0 - +500] */}
-              <span className="first-span">
-              <BsFillCloudDrizzleFill />  On {(moment(hour.timestamp_local).format('MMMM Do YYYY, h:mm:ss a'))} <BsFillBrightnessHighFill /><br /> 
+          <div className="modal-title">
+            {(moment(hour.timestamp_local).calendar())}
+          </div>
+          <div className="aqi-display">
+              <div className="aqi-display-number">
+                AQI<br />{hour.aqi}
+              </div>
+          </div>
+          <div className='next-five-container'>
+            <span className="next-five-display-number">
+                pm10<br />{hour.pm10}
+            </span>
+            <span className="next-five-display-number">
+                pm25<br />{hour.pm25}
+            </span>
+            <span className="next-five-display-number">
+                C0 <br /> {hour.co}
+            </span>
+            <span className="next-five-display-number">
+                N02 <br /> {hour.no2}
+            </span>
+            <span className="next-five-display-number">
+               03 <br /> {hour.o3}
+            </span>
+          </div>
+          {/* //2  Air Quality Index [US - EPA standard 0 - +500] */}
+          <span className="first-span">
+          <BsFillCloudDrizzleFill />  On {(moment(hour.timestamp_local).format('MMMM Do YYYY, h:mm:ss a'))} <BsFillBrightnessHighFill /><br /> 
                                         Air Quality Index : <span className={limit ? 'aqi-moderate': 'aqi-low'}>{hour.aqi}</span> <br /> 
                                         pm10: concentration of particulate matter smaller than 10 microns : {hour.pm10}.<br />
                                         pm25: concentration of particulate matter small than 25 micros: {hour.pm25}.<br />
                                         03: Trioxygen, the layer of ozone protecting the Earth from harmful rays.<br />
                                         NO2: More dangerous than Carbion Dioxide for the environment? 300% more potent.<br />
                                         CO: Carbon Monoxodie.{hour.co}  
-              </span>
-         
-        </div>
+          </span>        
       </div>
-    </>
+    
   );
 }
 
