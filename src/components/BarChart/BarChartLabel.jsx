8f7794f5651f39ddd5c5cc3a729436b457d331af
Franck Bushbaum <franck.bushbaum@yahoo.com>
2022-04-22 21:29:28 -0500

    Refresher/code-cleanup.

--- src/components/BarChart/BarChartLabel.jsx
+++ src/components/BarChart/BarChartLabel.jsx
@@ -18,17 +18,20 @@ function BarChartLabel({ hours }) {
     // const dispatch = useDispatch();
     // const onSprints = useSelector(store => store.fetchSprints)
 
-    const [color, setColor] = useState([]);
+    const [color, setColor] = useState();
     const [barAqi, setBarAqi] = useState([]);
 
-    const barAqis = hours.map((hour) => hour.aqi);
+  const barAqis = hours.map((hour) => hour.aqi);
 
-//4 Spread Operator??
-    const colorArray = () => {
-      let sumArray = [];
+
+//4 My first attempt at colorArray was a switch statement.  It wasn't working for some reason
+//4  I'm going to leave this like it is for now.
+
+  const colorArray = () => {
+    let sumArray = [];
       barAqis.forEach((aqi) => {
         if(0 < aqi && aqi < 20){
-          sumArray.push('#ff8b26')
+          sumArray.push('#26e6ff')
         } else if (20 <= aqi && aqi < 30){
           sumArray.push('#ffe600')
         } else if (30 <= aqi && aqi < 40){
@@ -50,6 +53,54 @@ function BarChartLabel({ hours }) {
       })
     }
 
+    // const colorArray = () => {
+    //   barAqis.forEach((aqi) => {
+    //     if(0 < aqi && aqi < 20){
+    //       setColor([...color, '#ff8b26'])
+    //     } else if (20 <= aqi && aqi < 30){
+    //       setColor([...color, '#ffe600'])
+    //     } else if (30 <= aqi && aqi < 40){
+    //       setColor([...color, 'rgb(34, 255, 0)'])
+    //     } else if (40 <= aqi && aqi < 50){
+    //       setColor([...color, 'rgb(216, 226, 28)'])
+    //     } else if (50 <= aqi && aqi < 60){
+    //       setColor([...color, 'rgb(66, 120, 181)'])
+    //     } else if (60 <= aqi && aqi < 70){
+    //       setColor([...color, 'rgb(111, 0, 255)'])
+    //     } else if (70 <= aqi && aqi < 80){
+    //       setColor([...color, 'rgb(0, 38, 255)'])
+    //     } else if (80 <= aqi && aqi < 90){
+    //       setColor([...color, 'rgb(204, 0, 255)'])
+    //     } else if (90 <= aqi && aqi < 100){
+    //       setColor([...color, 'rgb(255, 0, 162)'])
+    //     }
+    //   })
+    // }
+
+    // const colorArray = () => {
+    //   barAqis.forEach((aqi) => {
+    //     if(0 < aqi && aqi < 20){
+    //       setColor(['#ff8b26', ...color])
+    //     } else if (20 <= aqi && aqi < 30){
+    //       setColor(['#ffe600', ...color])
+    //     } else if (30 <= aqi && aqi < 40){
+    //       setColor(['rgb(34, 255, 0)', ...color])
+    //     } else if (40 <= aqi && aqi < 50){
+    //       setColor(['rgb(216, 226, 28)', ...color])
+    //     } else if (50 <= aqi && aqi < 60){
+    //       setColor(['rgb(66, 120, 181)', ...color])
+    //     } else if (60 <= aqi && aqi < 70){
+    //       setColor(['rgb(111, 0, 255)', ...color])
+    //     } else if (70 <= aqi && aqi < 80){
+    //       setColor(['rgb(0, 38, 255)', ...color])
+    //     } else if (80 <= aqi && aqi < 90){
+    //       setColor(['rgb(204, 0, 255)', ...color])
+    //     } else if (90 <= aqi && aqi < 100){
+    //       setColor(['rgb(255, 0, 162)', ...color])
+    //     }
+    //   })
+    // }
+
 
 
     const onData = {
@@ -58,7 +109,7 @@ function BarChartLabel({ hours }) {
             label: "AQI Every Hour",
             data: hours.map((hour) => hour.aqi),
             backgroundColor: color,
-            borderColor: [hours.length < 50 ? "#1FFF0F" : "#FFF01F"],
+            borderColor: "black",
             color: color,
             borderWidth: 2,
         }]
@@ -79,13 +130,11 @@ function BarChartLabel({ hours }) {
           },
           title: {
             display: true,
-            text: 'Chart.js Horizontal Bar Chart',
+            text: 'AQI',
             fontSize: 15,
           },
         },
       };
-
-    // console.log('What is hours?', hours.length)
 
     useEffect(() => {
         colorArray(); 
@@ -94,8 +143,7 @@ function BarChartLabel({ hours }) {
     return (
         <>
              <div className="bar-chart" style={{ height: "9em", width: "92%"}}>
-                <Bar options={options} data={onData} width={100}
-      height={18} />
+                <Bar options={options} data={onData} width={100} height={18} />
             </div>       
         </>
     );
