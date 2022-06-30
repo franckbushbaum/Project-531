6148e1d8b402b076ff92887d04ea268af23061f7
Franck Bushbaum <franck.bushbaum@yahoo.com>
2022-06-30 18:39:38 -0500

    centered table

--- src/components/MphChart/MphChart.jsx
+++ src/components/MphChart/MphChart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS } from 'chart.js/auto';
 import { useDispatch, useSelector } from 'react-redux';
